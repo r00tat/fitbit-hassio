3c500144b8a902b1ead77677deaca06430650709
Paul Woelfel <github@frig.at>
2020-12-28 21:14:14 +0100

    Fix hassio calls

--- companion/hassio.ts
+++ companion/hassio.ts
@@ -8,16 +8,27 @@ export class Hassio {
   async request(options: RequestInfo): Promise<any> {
     const { method = 'GET', url = '', body } = options;
     const headers = new Headers((options.headers || {}) as HeaderMap);
-    headers.set('Authentication', `Bearer ${this.token}`);
+    headers.set('Authorization', `Bearer ${this.token}`);
 
     console.info(`send hassio request ${this.baseUrl}${url}`);
-    const response = await fetch(`${this.baseUrl}${url}`, {
+    const fetchOptions: RequestInit = {
       method,
       headers,
       body: body && typeof body !== 'string' ? JSON.stringify(body) : body,
-    });
+    };
+    const response = await fetch(`${this.baseUrl}${url}`, fetchOptions);
     console.info(`hassio response: ${JSON.stringify(response)}`);
-    return await response.json();
+    console.info(`status: ${response.status}`);
+    const responseText = await response.text();
+    console.info(`text: ${responseText}`);
+    if (!response.ok) {
+      throw new Error(
+        `HTTP fetch failed ${response.status} ${response.statusText}: \n${responseText}`
+      );
+    }
+    const json = JSON.parse(responseText);
+    // console.info(`json response: ${JSON.stringify(json)}`);
+    return json;
   }
 
   async get(url: string): Promise<any> {
@@ -36,6 +47,6 @@ export class Hassio {
   }
 
   async callService(domain: string, service: string, data: any): Promise<any> {
-    return this.post(`/api/services/${domain}/${service}`, data);
+    return await this.post(`/api/services/${domain}/${service}`, data);
   }
 }
